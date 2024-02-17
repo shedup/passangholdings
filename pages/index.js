a92ae638234e8d9bbea5364a66de87fc69ebb47b
Tenzin <chaksamt@gmail.com>
2024-02-17 00:14:59 -0600

    styling issues

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,27 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <div>
       <main>
         <section id="feature-image">
-          <Image
+          <img
             className="profile-pic"
             src="/images/InfrontCongress.jpeg"
-            alt=""
+            alt="picture of a passang in front of the capital"
             width={300}
-            height={300}
           />
           <p>
-            {" "}
             Welcome to Passang Holdings. I will post my investment portofolio
-            here. Additionally I will write monthly{" "}
-            <a href="publication.html">publication</a>
+            here. Additionally I will write monthly
+            <Link href="/publication"> publications </Link>
             about my thoughts on the financail markets. Investment transactions
-            will be updated qaurterly. However, I don't expect any radical
+            will be updated quarterly. However, I don't expect any radical
             capital allocation since most of the investments are held for more
-            than five years.Also, I would love to hear from you, please find the
-            contact tab to send emails.
+            than five years. Also, I would love to hear from you, please find
+            the contact tab to send emails.
           </p>
         </section>
       </main>
