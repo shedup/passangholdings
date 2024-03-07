f9dc06555e19eccb5ba024cad86f9cf117286938
Tenzin <chaksamt@gmail.com>
2024-03-07 15:29:24 -0600

    adding it to publications

--- pages/publications.jsx
+++ pages/publications.jsx
@@ -6,8 +6,7 @@ import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
 
-export const revalidate = 0;
-
+export const dynamic = "force-dynamic";
 const publications = ({ posts }) => {
   const router = useRouter();
   let lists = JSON.parse(posts);
